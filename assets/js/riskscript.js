5023dfc6319a2ce18183b99437ddfe178031bb53
Hidnish <hideonishimura@live.it>
2021-11-11 20:08:44 +0000

    Add function to center content box on button click

--- assets/js/riskscript.js
+++ assets/js/riskscript.js
@@ -1,6 +1,13 @@
 function selectRiskTopic(selection) {
 
     $('.risk-content').addClass('hidden');
-    
     document.getElementById(selection).classList.remove('hidden');
 }
+
+$('.risk-btn').click(function () {
+    $('html, body').animate({
+        scrollTop: $('.risks-container').offset().top 
+    }, 500);
+    console.log('papaia');
+});
+
