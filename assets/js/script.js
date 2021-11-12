d285a57b1dc203d0e8916f434ffa07eddb82daf4
DeannaCarina <deannacarina@hotmail.com>
2021-11-12 10:36:40 +0000

    Add hero videos to index.html at large and small screen sizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     console.log("ciao");
 });
+
+var smallvid = document.getElementById("small-video");
+smallvid.playbackRate = 0.7; 
+
+var largevid = document.getElementById("large-video");
+largevid.playbackRate = 0.7; 
