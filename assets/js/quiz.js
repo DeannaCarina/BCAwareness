8e1f39c014470b53f2d4f2322e566940a1db9d8b
fernandagil <fernandagilgonzalez@gmail.com>
2021-11-13 21:29:39 +0000

    Create startGame function

---
+++ assets/js/quiz.js
@@ -0,0 +1,22 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
+
+function startGame () {
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+
+function setNextQuestion() {
+
+}
+
+
+function selectAnswer() {
+
+}
